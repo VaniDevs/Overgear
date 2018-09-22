c273d9224525255afb6bd8927d1e8ff19e6b1b6c
brian lee <brianhylee99@gmail.com>
2018-09-22 11:09:54 -0700

    remove C#-related elements from UI

--- Overgear/Overgear/Models/ErrorViewModel.cs
+++ Overgear/Overgear/Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace Overgear.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
