d46dcc239a6198a7d161dad4cd7b845a71844eab
ronnieh <ronnieh@sfu.ca>
2018-09-22 12:34:36 -0700

    Added new models

--- Overgear/Overgear/Models/Shirt.cs
+++ Overgear/Overgear/Models/Shirt.cs
@@ -5,7 +5,6 @@ namespace Overgear.Models
     {
         public int ID { get; set; }
         public string Description { get; set; }
-        public string Colour { get; set; }
         public int Size { get; set; }
         public int Quantity { get; set; }
     }
--- Overgear/Overgear/Models/Shoe.cs
+++ Overgear/Overgear/Models/Shoe.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 namespace Overgear.Models
 {
-    public class Boot
+    public class Headgear
     {
         public int ID { get; set; }
         public string Description { get; set; }
-        public string Colour { get; set; }
         public int Size { get; set; }
         public int Quantity { get; set; }
     }
