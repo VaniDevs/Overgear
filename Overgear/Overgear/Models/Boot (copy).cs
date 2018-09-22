cc99e6ae96910e15c64a1209cb47a98c206de1a6
ronnieh <ronnieh@sfu.ca>
2018-09-22 16:21:45 -0700

    Fixed navbar height

--- Overgear/Overgear/Models/Boot (copy).cs
+++ Overgear/Overgear/Models/Boot (copy).cs
@@ -3,11 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Overgear.Models
 {
-    public class Boot
+    public class ClientRequest
     {
         public int ID { get; set; }
-        public string Description { get; set; }
-        public string Colour { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
 
         [Range(0, Int32.MaxValue)]
         public int Size { get; set; }
