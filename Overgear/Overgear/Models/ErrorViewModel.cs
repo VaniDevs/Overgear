a7ae18099f6e902bdce71a8f2615b391791fa1d6
Carlo Conti <conti.carlo.1006@gmail.com>
2018-09-22 10:50:30 -0700

    Initial project commit

---
+++ Overgear/Overgear/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Overgear.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
